181713ceba382664878776b71b9bbe7517ee3b98
MuriloRussini <murilorussini@usp.br>
2022-06-08 00:22:02 -0300

    l

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var countDate = new Date("Dec 07, 2022 00:00:00").getTime();
+
+const newYear = () => {
+  var now = new Date().getTime();
+  gap = countDate - now;
+
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+
+  var d = Math.floor(gap / (day));
+  var h = Math.floor((gap % (day)) / (hour));
+  var m = Math.floor((gap % (hour)) / (minute));
+  var s = Math.floor((gap % (minute)) / (second));
+
+  document.getElementById('day').innerHTML = d;
+  document.getElementById('hour').innerHTML = h;
+  document.getElementById('minute').innerHTML = m;
+  document.getElementById('second').innerHTML = s;
+}
+
+setInterval (() => {
+  newYear();
+}, 1000);
